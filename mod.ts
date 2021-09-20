f3c9c98c53ba40ab64d9c453648163f119440320
MierenManz <christiaanmens@gmail.com>
2021-09-20 18:04:33 +0200

    add state

--- mod.ts
+++ mod.ts
@@ -1,18 +1,25 @@
 import { Application } from "./deps.ts";
-import { Lobby } from "./utils/lobbies.ts";
 import { lobbyRouter } from "./routers/lobby_router.ts";
 import { gameRouter } from "./routers/game_router.ts";
+import { resourceRouter } from "./routers/resource_router.ts";
+import { Cache } from "./utils/cache.ts";
 
-interface State {
-  lobbies: Map<string, Lobby>;
-}
+import type { ServerState } from "./types.ts";
 
-const app = new Application<State>({ state: { lobbies: new Map() } });
+const app = new Application<ServerState>({
+  state: {
+    lobbies: new Map(),
+    cache: new Cache(),
+  },
+});
 
 app.use(lobbyRouter.allowedMethods());
 app.use(lobbyRouter.routes());
 
 app.use(gameRouter.allowedMethods());
 app.use(gameRouter.routes());
+
+app.use(resourceRouter.allowedMethods());
+app.use(resourceRouter.routes());
 
 await app.listen({ port: 8000 });
--- types.ts
+++ types.ts
@@ -1,5 +1,7 @@
-import { Lobby } from "./utils/lobbies.ts";
+import type { Lobby } from "./utils/lobbies.ts";
+import type { Cache } from "./utils/cache.ts";
 
 export interface ServerState {
   lobbies: Map<string, Lobby>;
+  cache: Cache;
 }
