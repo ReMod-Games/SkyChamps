db24fee6d5b5cc465dbb8aac7180143c3c4825b3
MierenManz <christiaanmens@gmail.com>
2021-09-20 18:04:21 +0200

    remove unneeded deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,1 @@
-export { Err, match, Ok } from "https://deno.land/x/rustility@v0.0.1/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
-
-export type { Result } from "https://deno.land/x/rustility@v0.0.1/mod.ts";
