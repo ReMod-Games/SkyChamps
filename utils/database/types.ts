76915fae0f6c6c3714e7d8c65bd7103d737032c3
MierenManz <christiaanmens@gmail.com>
2021-09-24 23:55:00 +0200

    export more database types

--- utils/database/types.ts
+++ utils/database/types.ts
@@ -2,22 +2,22 @@
 import type { PreparedQuery } from "../../deps.ts";
 
 // Type aliases for clearer understanding
-type DateString = string;
-type MatchID = string;
-type PlayerID = 0 | 1;
-type PlayerName = string;
-type EventData = string;
-type Message = string;
-type CardID = number;
-type CardName = string;
-type CardDescription = string;
-type CardHealth = number;
-type CardAttackDamage = number;
-type CardAttackName = string;
-type CardAbilityName = string;
-type CardCritChance = number;
+export type DateString = string;
+export type MatchID = string;
+export type PlayerID = 0 | 1;
+export type PlayerName = string;
+export type EventData = string;
+export type Message = string;
+export type CardID = number;
+export type CardName = string;
+export type CardDescription = string;
+export type CardHealth = number;
+export type CardAttackDamage = number;
+export type CardAttackName = string;
+export type CardAbilityName = string;
+export type CardCritChance = number;
 
-interface MatchRecord extends Record<string, unknown> {
+export interface MatchRecord extends Record<string, unknown> {
   match_id: MatchID;
   started_at: DateString;
 }
@@ -29,7 +29,7 @@ interface MatchQueries {
   getMatchByID: GetMatchByID;
 }
 
-interface PlayerRecord extends Record<string, unknown> {
+export interface PlayerRecord extends Record<string, unknown> {
   player_id: PlayerID;
   player_name: PlayerName;
 }
@@ -41,7 +41,7 @@ interface MatchPlayerQueries {
   getPlayersByMatchID: GetPlayerByMatchID;
 }
 
-interface MessageRecord extends Record<string, unknown> {
+export interface MessageRecord extends Record<string, unknown> {
   date: DateString;
   message: Message;
   player_id: PlayerID;
@@ -55,7 +55,7 @@ interface ChatQueries {
   getMessagesByMatchID: GetMessagesByMatchID;
 }
 
-interface SpecialEventRecord extends Record<string, unknown> {
+export interface SpecialEventRecord extends Record<string, unknown> {
   date: DateString;
   event_data: EventData;
   player_id: PlayerID;
@@ -69,7 +69,7 @@ interface SpecialEventQueries {
   getEventsByMatchID: GetSpecialEventsByMatchID;
 }
 
-interface EventRecord extends Record<string, unknown> {
+export interface CardEventRecord extends Record<string, unknown> {
   date: DateString;
   card_id: CardID;
   event_data: EventData;
@@ -78,13 +78,13 @@ interface EventRecord extends Record<string, unknown> {
 
 // deno-fmt-ignore
 type AddCardEvent = PreparedQuery<[], never, [DateString, CardID, EventData, PlayerID, MatchID]>;
-type GetEventsByMatchID = PreparedQuery<[], EventRecord, [MatchID]>;
+type GetCardEventsByMatchID = PreparedQuery<[], CardEventRecord, [MatchID]>;
 interface CardEventQueries {
   addEvent: AddCardEvent;
-  getEventsByMatchID: GetEventsByMatchID;
+  getEventsByMatchID: GetCardEventsByMatchID;
 }
 
-interface CardRecord extends Record<string, unknown> {
+export interface CardRecord extends Record<string, unknown> {
   card_id: CardID;
   card_name: CardName;
   card_description: CardDescription;
