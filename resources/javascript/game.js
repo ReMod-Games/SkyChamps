841c35367c6c6ea22ce145ad3ed7f757a56203f1
MierenManz <christiaanmens@gmail.com>
2021-10-20 20:38:07 +0200

    remove unneeded things

---
+++ resources/javascript/game.js
@@ -0,0 +1,17 @@
+const name = prompt(
+  "What is your username",
+  Math.random().toString(36).substring(2, 9),
+);
+
+// Hacky but it works
+const gameID = document.location.pathname.split("/").pop();
+const ws = new WebSocket(
+  `ws://${document.location.host}/game/${gameID}/${name}`,
+);
+
+ws.onopen = console.error;
+ws.onclose = console.log; // Possibly Add this to chat?
+ws.onmessage = messageHandler;
+
+function messageHandler(evt) {
+}
