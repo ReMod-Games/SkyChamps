0456eaa411158d6db245edfce70585b84cb3d758
MierenManz <christiaanmens@gmail.com>
2021-10-21 21:10:47 +0200

    change some naming + notes

--- utils/match/game_state.ts
+++ utils/match/game_state.ts
@@ -1,16 +1,20 @@
-import type { CardRecord } from "../database/types.ts";
+import type { CardRecord as Card } from "../database/types.ts";
 
+type ModifiableKeys = "card_crit_chance" | "card_attack_dmg" | "card_health";
 export class GameState {
-  declare playerOneDeck: CardRecord[];
-  declare playerTwoDeck: CardRecord[];
+  // These deck's are already played
+  // Should be visible to all players
+  declare playerOneDeck: Card[];
+  declare playerTwoDeck: Card[];
 
   constructor() {
     this.playerOneDeck = [];
     this.playerTwoDeck = [];
   }
 
-  addCard(playerID: 0 | 1, card: CardRecord): number {
+  addCard(playerID: 0 | 1, card: Card): number {
     const deck = playerID === 0 ? this.playerOneDeck : this.playerTwoDeck;
+    // Note: This may need changes depending on client code.
     const possibleIndex = deck.findIndex((x) => x === undefined);
     const index = possibleIndex > 0 ? possibleIndex : deck.length;
     deck[index] = card;
@@ -22,11 +26,11 @@ export class GameState {
     delete deck[cardIndex];
   }
 
-  changeCard(
+  modifyCard(
     playerID: 0 | 1,
     cardIndex: number,
-    key: "card_crit_chance" | "card_attack_dmg" | "card_health",
-    value: CardRecord[typeof key],
+    key: ModifiableKeys,
+    value: Card[typeof key],
   ): boolean {
     const deck = playerID === 0 ? this.playerOneDeck : this.playerTwoDeck;
     const card = deck[cardIndex];
@@ -36,7 +40,7 @@ export class GameState {
     return true;
   }
 
-  getCard(playerID: 0 | 1, cardIndex: number): CardRecord | undefined {
+  getCard(playerID: 0 | 1, cardIndex: number): Card | undefined {
     const deck = playerID === 0 ? this.playerOneDeck : this.playerTwoDeck;
     return deck[cardIndex];
   }
