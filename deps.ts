1de999b452ad43699ab1b74c591475f13d151c73
MierenManz <christiaanmens@gmail.com>
2021-10-22 21:27:32 +0200

    update deps

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
 export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.1.1/mod.ts";
 
-export { ConsoleStream, Logger } from "https://deno.land/x/optic@1.3.0/mod.ts";
+export { ConsoleStream, Logger } from "https://deno.land/x/optic@1.3.1/mod.ts";
 export {
   every,
   FileStream,
-} from "https://deno.land/x/optic@1.3.0/streams/fileStream/mod.ts";
-export { TokenReplacer } from "https://deno.land/x/optic@1.3.0/formatters/mod.ts";
+} from "https://deno.land/x/optic@1.3.1/streams/fileStream/mod.ts";
+export { TokenReplacer } from "https://deno.land/x/optic@1.3.1/formatters/mod.ts";
 
 export type { Context } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
