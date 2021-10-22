1bb104d4fa24b6e4bdcafd61ff68a0c502b428d3
MierenManz <christiaanmens@gmail.com>
2021-10-22 23:14:24 +0200

    x

--- types.ts
+++ types.ts
@@ -1,7 +1,6 @@
 import type { Game } from "./utils/match/game.ts";
 import type { Cache } from "./utils/cache.ts";
 import type { Context } from "./deps.ts";
-import type { Card } from "./utils/cards/cards_cache.ts";
 
 interface ServerConfig {
   port: number;
@@ -19,5 +18,4 @@ export interface HTTPState extends StateBase {
 
 export interface WebSocketState extends StateBase {
   games: Map<string, Game>;
-  cardCache: Map<number, Card>;
 }
