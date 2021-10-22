40332945d7f86b24d640629ee0bb583484625348
MierenManz <christiaanmens@gmail.com>
2021-10-22 21:21:56 +0200

    state

--- types.ts
+++ types.ts
@@ -1,9 +1,16 @@
 import type { Game } from "./utils/match/game.ts";
 import type { Cache } from "./utils/cache.ts";
 import type { Context } from "./deps.ts";
+import type { Card } from "./utils/cards/cards_cache.ts";
+
+interface ServerConfig {
+  port: number;
+  protocol: "tcp";
+}
 
 interface StateBase {
   tracker(ctx: Context): void;
+  serverConfig: ServerConfig;
 }
 
 export interface HTTPState extends StateBase {
@@ -12,4 +19,5 @@ export interface HTTPState extends StateBase {
 
 export interface WebSocketState extends StateBase {
   games: Map<string, Game>;
+  cardCache: Map<number, Card>;
 }
--- websocket_server.ts
+++ websocket_server.ts
@@ -1,6 +1,7 @@
 import { Application } from "./deps.ts";
 import { gameRouter } from "./routers/game_router.ts";
 import { logger, tracker } from "./utils/logger.ts";
+import { cardCache } from "./utils/cards/cards_cache.ts";
 
 import type { WebSocketState } from "./types.ts";
 
@@ -9,6 +10,11 @@ const wsServer = new Application<WebSocketState>({
   state: {
     tracker,
     games: new Map(),
+    cardCache,
+    serverConfig: {
+      port: 8001,
+      protocol: "tcp",
+    },
   },
 });
 
@@ -16,4 +22,4 @@ wsServer.addEventListener("error", (e) => logger.error(e) as unknown as void);
 wsServer.use(gameRouter.allowedMethods());
 wsServer.use(gameRouter.routes());
 
-await wsServer.listen({ port: 8001 });
+await wsServer.listen(wsServer.state.serverConfig);
