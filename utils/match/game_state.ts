5d56c70cf51b665eec4eb9aeb57cb3597a9eb0db
MierenManz <christiaanmens@gmail.com>
2021-10-26 16:39:32 +0200

    clean up game_state.ts

--- utils/match/game_state.ts
+++ utils/match/game_state.ts
@@ -4,28 +4,24 @@ export class GameState {
   public turn: number;
   // These deck's are already played
   // Should be visible to all players
-  public playerOneDeck: Deck;
-  public playerTwoDeck: Deck;
-
+  public playerDecks: Deck[];
   constructor() {
     this.turn = 0;
-    this.playerOneDeck = new Deck();
-    this.playerTwoDeck = new Deck();
+    this.playerDecks = [new Deck(), new Deck()];
   }
 
   nextTurn() {
-    for (const card of this.playerOneDeck) {
-      card.executeTurnActions(this.turn);
-    }
-    for (const card of this.playerTwoDeck) {
-      card.executeTurnActions(this.turn);
+    for (const deck of this.playerDecks) {
+      for (const card of deck) {
+        card.executeTurnActions(this.turn);
+      }
     }
 
     this.turn++;
   }
 
   cleanUp(): void {
-    this.playerOneDeck.cleanUp();
-    this.playerTwoDeck.cleanUp();
+    this.playerDecks[0].cleanUp();
+    this.playerDecks[1].cleanUp();
   }
 }
