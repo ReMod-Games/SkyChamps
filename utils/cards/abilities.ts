d373d32e1286998c2eec5db9de72799de4ba6cca
MierenManz <christiaanmens@gmail.com>
2021-10-26 19:23:49 +0200

    abilities

---
+++ utils/cards/abilities.ts
@@ -0,0 +1,17 @@
+import type { Card } from "./card.ts";
+
+export function wholesomeme(_card: Card) {}
+export function dash(_card: Card) {}
+export function reroute(_card: Card) {}
+export function squirtle(_card: Card) {}
+export function salterminate(_card: Card) {}
+export function wysi(_card: Card) {}
+export function xeno(_card: Card) {}
+export function glitch(_card: Card) {}
+export function wheatwhat(_card: Card) {}
+export function transform(_card: Card) {}
+export function healingbeam(_card: Card) {}
+export function hackcard(_card: Card) {}
+export function hyper(_card: Card) {}
+export function gasolineleak(_card: Card) {}
+export function soundofheath(_card: Card) {}
