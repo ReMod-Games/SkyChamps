f4a6e2d0a19bca3a1220c63465e4d76de084b774
MierenManz <christiaanmens@gmail.com>
2021-10-26 15:30:42 +0200

    fix small oversight

--- utils/match/validate_record.ts
+++ utils/match/validate_record.ts
@@ -8,21 +8,19 @@ type UnionToTuple<T> = UnionToIntersection<
 > extends (_: never) => infer W ? [...UnionToTuple<Exclude<T, W>>, W]
   : Array<T>;
 
-export function validateRecord<
-  T,
->(
+export function validateRecord<T>(
   object: T,
   validationArray: UnionToTuple<keyof T>,
 ): boolean {
   // Check if length is the same.
   // Either required keys are not present.
   // Or illegal keys have been added.
-  const keys = Object.keys(object);
+  const keys = Object.keys(object) as Array<keyof T>;
   if (keys.length !== validationArray.length) return false;
 
   // Check all keys to see if they're legal.
-  for (const key in keys) {
-    if (!validationArray.includes(key as keyof T)) return false;
+  for (const key of keys) {
+    if (!validationArray.includes(key)) return false;
   }
 
   return true;
