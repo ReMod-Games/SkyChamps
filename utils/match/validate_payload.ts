e33152c6e4c2b948285dfb9eb6866a06b0b7813d
MierenManz <christiaanmens@gmail.com>
2021-11-01 21:47:50 +0100

    rename

--- utils/match/validate_payload.ts
+++ utils/match/validate_payload.ts
@@ -8,14 +8,14 @@ type UnionToTuple<T> = UnionToIntersection<
 > extends (_: never) => infer W ? [...UnionToTuple<Exclude<T, W>>, W]
   : Array<T>;
 
-export function validateRecord<T>(
-  object: T,
+export function isValidPayload<T>(
+  payload: T,
   validationArray: UnionToTuple<keyof T>,
 ): boolean {
   // Check if length is the same.
   // Either required keys are not present.
   // Or illegal keys have been added.
-  const keys = Object.keys(object) as Array<keyof T>;
+  const keys = Object.keys(payload) as Array<keyof T>;
   if (keys.length !== validationArray.length) return false;
 
   // Check all keys to see if they're legal.
