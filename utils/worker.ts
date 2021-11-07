56edeefe5f4ce430107edaac611751ffaf802a02
MierenManz <christiaanmens@gmail.com>
2021-11-07 16:06:51 +0100

    fix: guarantee a worker get's terminated when erroring

--- utils/worker.ts
+++ utils/worker.ts
@@ -1,12 +1,13 @@
 import { logger } from "./logger.ts";
 
 export function startWorker(workerURL: URL, options: WorkerOptions) {
-  logger.debug(`${options.name} Starting!`);
+  logger.debug(`Worker "${options.name}" Starting!`);
   const worker = new Worker(workerURL, options);
   worker.onerror = (error) => {
     error.preventDefault();
     logger.critical(`Worker "${options.name}" has crashed!`);
     logger.critical(`Restarting "${options.name}" now!`);
+    worker.terminate();
     startWorker(workerURL, options);
   };
 }
