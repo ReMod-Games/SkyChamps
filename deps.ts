173852e6a34e5ee19e452e847d2311927afca322
MierenManz <christiaanmens@gmail.com>
2021-11-09 17:16:42 +0100

    fix deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,10 @@
 export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.1.2/mod.ts";
-export { ConsoleStream, Logger } from "https://deno.land/x/optic@1.3.1/mod.ts";
+export {
+  ConsoleStream,
+  Level,
+  Logger,
+} from "https://deno.land/x/optic@1.3.1/mod.ts";
 export {
   every,
   FileStream,
@@ -9,4 +13,3 @@ export { TokenReplacer } from "https://deno.land/x/optic@1.3.1/formatters/mod.ts
 
 export type { Context } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export type { PreparedQuery } from "https://deno.land/x/sqlite@v3.1.2/mod.ts";
-export type { Level } from "https://deno.land/x/optic@1.3.1/mod.ts";
