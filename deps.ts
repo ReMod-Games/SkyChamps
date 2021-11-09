bd75f65c2468e22febee5237f3470f3f273196db
MierenManz <christiaanmens@gmail.com>
2021-11-09 16:51:32 +0100

    add deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.1.2/mod.ts";
-
 export { ConsoleStream, Logger } from "https://deno.land/x/optic@1.3.1/mod.ts";
 export {
   every,
@@ -9,5 +8,5 @@ export {
 export { TokenReplacer } from "https://deno.land/x/optic@1.3.1/formatters/mod.ts";
 
 export type { Context } from "https://deno.land/x/oak@v9.0.1/mod.ts";
-
 export type { PreparedQuery } from "https://deno.land/x/sqlite@v3.1.2/mod.ts";
+export type { Level } from "https://deno.land/x/optic@1.3.1/mod.ts";
