2d1d8b8d35907ee5edd1ebb17be126a3f96f92c2
MierenManz <christiaanmens@gmail.com>
2021-11-09 09:56:28 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
-export { DB } from "https://deno.land/x/sqlite@v3.1.1/mod.ts";
+export { DB } from "https://deno.land/x/sqlite@v3.1.2/mod.ts";
 
 export { ConsoleStream, Logger } from "https://deno.land/x/optic@1.3.1/mod.ts";
 export {
@@ -10,4 +10,4 @@ export { TokenReplacer } from "https://deno.land/x/optic@1.3.1/formatters/mod.ts
 
 export type { Context } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
-export type { PreparedQuery } from "https://deno.land/x/sqlite@v3.1.1/mod.ts";
+export type { PreparedQuery } from "https://deno.land/x/sqlite@v3.1.2/mod.ts";
