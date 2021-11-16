ebee4d41bb6f170597db2179168ef3cf2843315c
MierenManz <christiaanmens@gmail.com>
2021-11-16 16:39:10 +0100

    fmt

--- utils/jit_compile.ts
+++ utils/jit_compile.ts
@@ -4,9 +4,9 @@ import { log } from "./worker_logger.ts";
 const encoder = new TextEncoder();
 
 export function compile(base: string, entry: Deno.DirEntry): Uint8Array {
-  log(20,`Reading ${entry.name}`);
+  log(20, `Reading ${entry.name}`);
   const tsCode = Deno.readTextFileSync(base + entry.name);
-  log(20,`Parsing, Tranforming & Encoding ${entry.name}`);
+  log(20, `Parsing, Tranforming & Encoding ${entry.name}`);
   return encoder.encode(
     swcTransform(tsCode, {
       // @ts-ignore: Typings are fucked...
