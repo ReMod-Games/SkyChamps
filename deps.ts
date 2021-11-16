830b262bae02c850cfbb2d613c9b6ab026785b23
MierenManz <christiaanmens@gmail.com>
2021-11-16 15:50:56 +0100

    fix: Actually transpile typescript

--- deps.ts
+++ deps.ts
@@ -11,8 +11,7 @@ export {
 } from "https://deno.land/x/optic@1.3.1/streams/fileStream/mod.ts";
 export { TokenReplacer } from "https://deno.land/x/optic@1.3.1/formatters/mod.ts";
 export {
-  parse as swcParse,
-  print as swcPrint,
+  transform as swcTransform,
 } from "https://deno.land/x/swc@0.1.4/mod.ts";
 
 export type { Context } from "https://deno.land/x/oak@v9.0.1/mod.ts";
--- utils/jit_compile.ts
+++ utils/jit_compile.ts
@@ -1,4 +1,4 @@
-import { swcParse, swcPrint } from "../deps.ts";
+import { swcTransform } from "../deps.ts";
 import { logger } from "./logger.ts";
 
 const encoder = new TextEncoder();
@@ -6,22 +6,16 @@ const encoder = new TextEncoder();
 export function compile(base: string, entry: Deno.DirEntry): Uint8Array {
   logger.debug(`Reading ${entry.name}`);
   const tsCode = Deno.readTextFileSync(base + entry.name);
-  logger.debug(`Parsing ${entry.name}`);
-  const ast = swcParse(
-    tsCode,
-    {
-      syntax: "typescript",
-      comments: false,
-      target: "es2020",
-    },
-  );
-  logger.debug(`Printing & Encoding ${entry.name}`);
-
+  logger.debug(`Parsing, Tranforming & Encoding ${entry.name}`);
   return encoder.encode(
-    swcPrint(ast, {
+    swcTransform(tsCode, {
+      // @ts-ignore: Typings are fucked...
       minify: true,
       jsc: {
         target: "es2020",
+        parser: {
+          syntax: "typescript",
+        },
       },
     }).code,
   );
