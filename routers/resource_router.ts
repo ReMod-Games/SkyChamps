e5bd1f1a8a794350260222cdb867be337e1112ca
MierenManz <christiaanmens@gmail.com>
2021-11-18 21:41:56 +0100

    fix stupid stuff

--- routers/resource_router.ts
+++ routers/resource_router.ts
@@ -5,24 +5,29 @@ const resourceRouter = new Router<HTTPState>({
   prefix: "/resources",
 });
 
-resourceRouter.use((ctx) => ctx.state.tracker(ctx));
-
 resourceRouter.get("/css/:file", function (ctx) {
+  ctx.state.tracker(ctx);
+  console.log(ctx.params.file);
   ctx.response.body = ctx.state.cache.get(
     `./frontend/css/${ctx.params.file}`,
   );
+  ctx.response.type = "text/css";
 });
 
 resourceRouter.get("/images/:file", function (ctx) {
+  ctx.state.tracker(ctx);
   ctx.response.body = ctx.state.cache.get(
     `./frontend/images/${ctx.params.file}`,
   );
+  ctx.response.type = "image/png";
 });
 
 resourceRouter.get("/javascript/:file", function (ctx) {
+  ctx.state.tracker(ctx);
   ctx.response.body = ctx.state.cache.get(
     `./frontend/typescript/${ctx.params.file}`,
   );
+  ctx.response.type = "text/javascript"
 });
 
 export { resourceRouter };
