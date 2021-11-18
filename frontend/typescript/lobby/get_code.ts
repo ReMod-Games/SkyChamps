dd46cc2c169f492c8c05d20ba69a291123d58318
MierenManz <christiaanmens@gmail.com>
2021-11-18 23:08:57 +0100

    fix cors

--- frontend/typescript/lobby/get_code.ts
+++ frontend/typescript/lobby/get_code.ts
@@ -1,7 +1,7 @@
 /// <reference lib="dom"/>
 
 onload = async () => {
-  const id = await fetch(`http://localhost:8001/get_code`).then((res) =>
+  const id = await fetch(`http://localhost:8001/get_code`, {mode: "no-cors"}).then((res) =>
     res.text()
   );
   location.replace(`${location.origin}/lobby/${id}`);
