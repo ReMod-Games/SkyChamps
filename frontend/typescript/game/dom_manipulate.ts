884ffed466a9837211e6099106d6880ec1b39d1d
MierenManz <christiaanmens@gmail.com>
2021-11-21 22:57:00 +0100

    dom stuff

--- frontend/typescript/game/dom_manipulate.ts
+++ frontend/typescript/game/dom_manipulate.ts
@@ -1,1 +1,26 @@
 /// <reference lib="dom"/>
+
+export function setTimer(timeLeft: number, interval: number): number {
+  const timer = document.getElementById("timer")!;
+  let timeRemaining = timeLeft;
+  
+  return setInterval(
+    () => timer.innerText = (timeRemaining -= interval).toString(),
+    interval
+  );
+}
+
+export function setPlayername(name: string): void {
+  const user = document.getElementById("username")!;
+  user.innerText = name;
+}
+
+export function addChatMessage(message: string, player: string) {
+  const chat = document.getElementById("chat_box")!;
+  chat.innerHTML += `<p>${player}: ${message.replace(/[\r-\n]/, "<br>")}</p>`;
+}
+
+export function addErrorMessage(message: string) {
+  const chat = document.getElementById("chat_box")!;
+  chat.innerHTML+= `<p class="error">${message}</p>`;
+}
