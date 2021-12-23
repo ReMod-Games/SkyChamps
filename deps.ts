70c23d8a3d9987b92f3318f0af32f5b055c1cf89
MierenManz <christiaanmens@gmail.com>
2021-12-23 12:05:50 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { Application, Router } from "https://deno.land/x/oak@v10.0.0/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 export {
   ConsoleStream,
   Level,
@@ -13,6 +13,6 @@ export {
   transform as swcTransform,
 } from "https://deno.land/x/swc@0.1.4/mod.ts";
 
-export { walk } from "https://deno.land/std@0.115.1/fs/walk.ts";
+export { walk } from "https://deno.land/std@0.119.0/fs/walk.ts";
 
-export type { Context } from "https://deno.land/x/oak@v10.0.0/mod.ts";
+export type { Context } from "https://deno.land/x/oak@v10.1.0/mod.ts";
