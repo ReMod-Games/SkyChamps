cf918f5eeca87a3bcd2e3b3dbf69d3e2c7942aaa
MierenManz <christiaanmens@gmail.com>
2021-12-30 20:23:57 +0100

    more misc

--- frontend/typescript/game/dom_manipulate.ts
+++ frontend/typescript/game/dom_manipulate.ts
@@ -1,5 +1,5 @@
 /// <reference lib="dom"/>
-
+import type { CardJson } from "../../../types/card.ts";
 export function setTimer(timeLeft: number, interval: number): number {
   const timer = document.getElementById("timer")!;
   let timeRemaining = timeLeft;
@@ -17,10 +17,24 @@ export function setPlayername(name: string): void {
 
 export function addChatMessage(message: string, player: string) {
   const chat = document.getElementById("chat_box")!;
-  chat.innerHTML += `<p>${player}: ${message.replace(/[\r-\n]/, "<br>")}</p>`;
+  chat.innerHTML += `<p>${player}: ${message.replace(/[\r\n]/, "<br>")}</p>`;
 }
 
 export function addErrorMessage(message: string) {
   const chat = document.getElementById("chat_box")!;
   chat.innerHTML += `<p class="error">${message}</p>`;
+}
+
+export function addToDeck(player: "opp"): void;
+export function addToDeck(player: "self", card: CardJson): void;
+export function addToDeck(player: "self" | "opp", card?: CardJson): void {
+  const deck = document.getElementById(player + "_deck");
+  if (!deck) return;
+  const cardDiv = document.createElement("div");
+  if (card) {
+    cardDiv.innerHTML = Object.entries(card)
+      .reduce((str, [key, value]) => str += `${key}: ${value}<br/>`, "");
+  } else cardDiv.innerHTML = "hidden";
+
+  deck.appendChild(cardDiv);
 }
