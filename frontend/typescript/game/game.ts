b2d5ed56f907a07201782611461d7f63819b8fa9
MierenManz <christiaanmens@gmail.com>
2022-01-02 22:38:56 +0100

    fix: thingy

--- frontend/typescript/game/game.ts
+++ frontend/typescript/game/game.ts
@@ -5,7 +5,7 @@ import { messageHandler } from "./game_logic.js";
 const [gameID, username] = location.pathname.replace("/lobby/", "").split("/");
 
 const socket = new WebSocket(
-  `ws://${location.origin.replace("8000", "8001")}/${gameID}/${username}`,
+  `ws://${location.hostname}:8001/${gameID}/${username}`,
 );
 await new Promise((res) => socket.onopen = res);
 socket.onmessage = messageHandler;
