abcc7063251e49db85cf1b20f750888c5d6ee3c3
MierenManz <christiaanmens@gmail.com>
2022-01-03 12:53:20 +0100

    move eventlisteners

--- frontend/typescript/game/game.ts
+++ frontend/typescript/game/game.ts
@@ -12,35 +12,47 @@ const [gameID, username] = location.pathname.replace("/lobby/", "").split("/");
 const socket = new WebSocket(
   `ws://${location.hostname}:8001/${gameID}/${username}`,
 );
-await new Promise((res) => socket.onopen = res);
+const connectionPromise = new Promise((res) => socket.onopen = res);
 socket.onmessage = messageHandler;
 
-document.getElementById("chat_input")!.onsubmit = (evt) => {
-  const message = (evt.submitter as HTMLInputElement).value;
-  const struct: MiscEvents.ChatMessage = {
-    type: "chat_message",
-    message,
-    user: username,
-  };
-  (evt.submitter as HTMLInputElement).value = "";
-  socket.send(JSON.stringify(struct));
-};
+onload = () => {
+  console.log("REEEEE");
+  GAME_STATE.opp.element = document.getElementById(
+    "opp_deck",
+  )! as HTMLDivElement;
+  GAME_STATE.self.element = document.getElementById(
+    "self_deck",
+  )! as HTMLDivElement;
 
-document.getElementById("attack")!.onclick = () => {
-  const { opp: oppIndex, self: selfIndex } = GAME_STATE.hightlightedCards;
-  if (!selfIndex) {
-    addErrorMessage("Please select a card to attack with");
-    return;
-  }
-  if (!oppIndex) {
-    addErrorMessage("Please select a card to attack");
-    return;
-  }
-  const payload: GameEvents.Attack = {
-    type: "attack",
-    attackerCardIndex: selfIndex,
-    defenderCardIndex: oppIndex,
+  document.getElementById("chat_input")!.onsubmit = (evt) => {
+    const message = (evt.submitter as HTMLInputElement).value;
+    const struct: MiscEvents.ChatMessage = {
+      type: "chat_message",
+      message,
+      user: username,
+    };
+    (evt.submitter as HTMLInputElement).value = "";
+    socket.send(JSON.stringify(struct));
   };
 
-  socket.send(JSON.stringify(payload));
+  document.getElementById("attack")!.onclick = () => {
+    const { opp: oppIndex, self: selfIndex } = GAME_STATE.hightlightedCards;
+    if (!selfIndex) {
+      addErrorMessage("Please select a card to attack with");
+      return;
+    }
+    if (!oppIndex) {
+      addErrorMessage("Please select a card to attack");
+      return;
+    }
+    const payload: GameEvents.Attack = {
+      type: "attack",
+      attackerCardIndex: selfIndex,
+      defenderCardIndex: oppIndex,
+    };
+
+    socket.send(JSON.stringify(payload));
+  };
 };
+
+await connectionPromise;
