0edde918ab647599f122f1adfbd48e59fff2cf9d
MierenManz <christiaanmens@gmail.com>
2022-01-03 22:52:55 +0100

    add more keys to display

--- frontend/typescript/game/util.ts
+++ frontend/typescript/game/util.ts
@@ -2,12 +2,14 @@
 
 import type { CardJson } from "../../../types/card.ts";
 
+const EXCLUDELIST = ["description", "abilityname", "critchance"];
+
 export function createCard(card?: CardJson): HTMLDivElement {
   const cardDiv = document.createElement("div");
   cardDiv.setAttribute("class", "card");
   if (card) {
     cardDiv.innerHTML = Object.entries(card)
-      .filter(([key]) => key.toLowerCase().includes("name"))
+      .filter(([key]) => !EXCLUDELIST.includes(key.toLowerCase()))
       .reduce((str, [key, value]) => str += `${key}: ${value}<br/>`, "");
   } else cardDiv.innerHTML = "hidden";
   return cardDiv;
