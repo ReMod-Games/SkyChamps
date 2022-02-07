23ed018bb90d4b631db04eccc45dba3b9772cd6d
MierenManz <christiaanmens@gmail.com>
2022-02-07 21:08:00 +0100

    use defer

--- frontend/typescript/index/index.ts
+++ frontend/typescript/index/index.ts
@@ -1,17 +1,15 @@
 /// <reference lib="dom"/>
 
-onload = () => {
-  document.getElementById("create_match")!.onclick = () => {
-    fetch(`${location.origin}/get_code`)
-      .then((res) => res.text())
-      .then((id) => redirect(`${location.origin}/lobby/${id}`));
-  };
+document.getElementById("create_match")!.onclick = () => {
+  fetch(`${location.origin}/get_code`)
+    .then((res) => res.text())
+    .then((id) => redirect(`${location.origin}/lobby/${id}`));
+};
 
-  document.getElementById("join_match")!.onclick = () => {
-    const id = prompt("Please enter your match id");
-    if (id) redirect(`${location.origin}/lobby/${id}`);
-    else alert("Invalid ID");
-  };
+document.getElementById("join_match")!.onclick = () => {
+  const id = prompt("Please enter your match id");
+  if (id) redirect(`${location.origin}/lobby/${id}`);
+  else alert("Invalid ID");
 };
 
 function redirect(url: string) {
