db6d7c08a88b44c9fe2cc9f60fcd360d990cd891
MierenManz <christiaanmens@gmail.com>
2022-02-07 18:12:34 +0100

    database

--- utils/database/types.ts
+++ utils/database/types.ts
@@ -1,18 +1,45 @@
-// import type { PreparedQuery } from "../../deps.ts";
+// import { SQLiteDatabase } from "../../deps.ts";
+import type { PreparedQuery } from "../../deps.ts";
 
-// interface Queries {
-//   replays: PreparedQuery;
-//   chatMessages: PreparedQuery;
-//   matches: PreparedQuery;
-// }
+type R = Record<string, unknown>;
 
-// export interface Statements {
-//   select: {
-//     replays: PreparedQuery;
-//     chatMessages: PreparedQuery;
-//     matches: PreparedQuery;
-//   };
-//   insert: {
-//     replays: PreparedQuery;
-//   };
-// }
+type ToQueryArray<T> = T[keyof T][];
+type ToPreparedQuery<T extends R> = PreparedQuery<ToQueryArray<T>, T>;
+
+interface Action extends R {
+  matchID: number;
+  user: string;
+  action: Record<string, string>;
+  date: Date;
+}
+
+interface Message extends R {
+  matchID: number;
+  user: string;
+  message: string;
+  date: Date;
+}
+
+interface Replay extends R {
+  matchID: number;
+  data: Record<string, string>;
+}
+
+interface Match extends R {
+  matchID: number;
+  date: Date;
+  status: 0 | 1 | 2;
+}
+
+export interface Queries {
+  matches: ToPreparedQuery<Match>;
+  actions: ToPreparedQuery<Action>;
+  messages: ToPreparedQuery<Message>;
+  replays: ToPreparedQuery<Replay>;
+}
+
+export interface Statements {
+  select: Queries;
+  insert: Queries;
+  update: Queries;
+}
