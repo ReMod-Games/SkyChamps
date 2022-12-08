bda3516644357f1769876809306c59725c278509
MierenManz <christiaanmens@gmail.com>
2022-12-08 16:25:49 +0100

    yeet

--- types/client_send_payloads/mod.ts
+++
@@ -1,7 +0,0 @@
-export * as MiscEvents from "./misc.ts";
-export * as GameEvents from "./game.ts";
-
-import type { AnyMiscEvent } from "./misc.ts";
-import type { AnyGameEvent } from "./game.ts";
-
-export type AnyClientEvent = AnyMiscEvent | AnyGameEvent;
--- types/server_send_payloads/mod.ts
+++
@@ -1,9 +0,0 @@
-export * as OppEvents from "./opponent.ts";
-export * as SelfEvents from "./self.ts";
-export * as MiscEvents from "./misc.ts";
-
-import type { AnyMiscEvent } from "./misc.ts";
-import type { AnyOppEvent } from "./opponent.ts";
-import type { AnySelfEvent } from "./self.ts";
-
-export type AnyServerEvent = AnyMiscEvent | AnyOppEvent | AnySelfEvent;
