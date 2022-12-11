16aa29873dbc5b35ead9eef768eb435e4af4c0bd
MierenManz <christiaanmens@gmail.com>
2022-12-12 00:02:21 +0100

    clean up install script (unfinished)

--- scripts/install.ts
+++ scripts/install.ts
@@ -1,12 +1,10 @@
 import { requires, run } from "./_util.ts";
 
-export async function install() {
-  if (!await requires("git")) {
-    console.error("Missing any of the following shell commands: [git]");
-    Deno.exit(1);
-  }
+export default async function install() {
+  await requires("git");
 
   await run("git", "clone", "https://github.com/remod-games/skychamps.git");
+
   Deno.chdir("./skychamps");
 
   const CONFIG_BASE = {
